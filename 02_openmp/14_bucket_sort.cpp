1eab2407a99364b939d1eaff30a7775ced73acc6
Koki Ikeda <ikeda.k@ic.c.titech.ac.jp>
2023-04-22 18:33:26 +0900

    第2回課題提出

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -6,18 +6,34 @@ int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
+#pragma omp parallel for shared(key)
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
+  std::vector<int> bucket(range,0);
+#pragma omp parallel for shared(bucket)
   for (int i=0; i<n; i++)
+#pragma omp atomic update
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  std::vector<int> offset_tmp(range, 0);
+#pragma omp parallel for shared(offset)
+  for (int i=1; i<range;i++)
+      offset[i] = bucket[i-1];
+#pragma omp parallel shared(offset)
+  for (int j=1;j<range;j<<=1) {
+#pragma omp for
+    for (int i=0;i<range;i++) {
+      offset_tmp[i] = offset[i];
+    }
+#pragma omp for
+    for (int i=j;i<range;i++)
+      offset[i] += offset_tmp[i-j];
+  }
+#pragma omp parallel for shared(key)
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
